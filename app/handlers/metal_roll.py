c633595bfd3841d990f27b7cf90e4113db14d4ff
Anton <patrin_toxa@mail.ru>
2025-03-13 17:56:28 +0500

    feat: import files and take delete_metal_roll router

--- app/handlers/metal_roll.py
+++ app/handlers/metal_roll.py
@@ -1,8 +1,9 @@
 from typing import Annotated
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 
 from app.dependecy import get_metal_roll_service
+from app.exception import MetalRollNotFoundException
 from app.schemas.metal_roll import MetalRollCreate, MetalRollInfo
 from app.service.metal_roll import MetalRollService
 
@@ -29,3 +30,18 @@ async def create_metal_roll(
     ],
 ) -> MetalRollInfo:
     return await metal_roll_serv.create_metal_roll(data)
+
+
+@router.delete("/{metal_roll_id}/delete")
+async def delete_metal_roll(
+    metal_roll_id: int,
+    metal_roll_serv: Annotated[
+        MetalRollService, Depends(get_metal_roll_service)
+    ],
+) -> MetalRollInfo:
+    try:
+        return await metal_roll_serv.delete_metal_roll(
+            metal_roll_id=metal_roll_id
+        )
+    except MetalRollNotFoundException as e:
+        raise HTTPException(status_code=404, detail=e.detail)
