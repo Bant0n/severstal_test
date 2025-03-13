19bc3c54ac3d6a4856907604ac74ba624003487f
Anton <patrin_toxa@mail.ru>
2025-03-13 17:07:59 +0500

    init package

--- app/schemas/__init__.py
+++ app/schemas/__init__.py
@@ -1,3 +1,3 @@
-from .metal_roll import MetalRoll, MetalRollBase, MetalRollCreate
+from .metal_roll import MetalRollBase, MetalRollCreate, MetalRollInfo
 
 __all__ = ["MetalRoll", "MetalRollBase", "MetalRollCreate"]
