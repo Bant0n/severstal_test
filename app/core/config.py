81614939029d0c26392d532bf13b44839de89419
Anton <patrin_toxa@mail.ru>
2025-03-13 15:59:39 +0500

    feat: add settings of dev

---
+++ app/core/config.py
@@ -0,0 +1,18 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_DB: str
+    POSTGRES_HOST: str
+    POSTGRES_PORT: int
+
+    @property
+    def db_url(self):
+        return f"postgresql+asyncpg://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}@{self.POSTGRES_HOST}:{self.POSTGRES_PORT}/{self.POSTGRES_DB}"  # noqa
+
+    model_config = SettingsConfigDict(env_file=".env")
+
+
+settings = Settings()
