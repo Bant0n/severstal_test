717f42547e09e22eca3f8680e0562a1adecad59d
Anton <patrin_toxa@mail.ru>
2025-03-13 17:55:41 +0500

    feat: add _get_single_metal_roll method

--- app/repository/metal_roll.py
+++ app/repository/metal_roll.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass
 
-from sqlalchemy import select
+from sqlalchemy import delete, select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.models.metal_roll import MetalRoll
@@ -18,7 +18,21 @@ class MetalRollRepository:
             await session.commit()
             return metal_roll
 
+    async def _get_single_metal_roll(self, metal_roll_id: int):
+        async with self.db_session as session:
+            result = await session.execute(
+                select(MetalRoll).where(MetalRoll.id == metal_roll_id)
+            )
+            return result.scalars().first()
+
     async def get_all_metal_rolls(self):
         async with self.db_session as session:
             result = await session.execute(select(MetalRoll))
             return result.scalars().all()
+
+    async def delete_metal_roll(self, metal_roll_id: int):
+        async with self.db_session as session:
+            await session.execute(
+                delete(MetalRoll).where(MetalRoll.id == metal_roll_id)
+            )
+            await session.commit()
